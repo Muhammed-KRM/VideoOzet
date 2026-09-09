7582769004d5c023634dc2facd7451b05fd79eb6
Muhammed-KRM <ustunmuhammed09@gmil.com>
2026-09-09 14:20:44 +0300

    Faz 4 Unit Testleri eklendi - ExtractTranscriptConsumer (3 test, hepsi basarili)

--- src/VideoOzet.Data/Context/AppDbContext.cs
+++ src/VideoOzet.Data/Context/AppDbContext.cs
@@ -11,25 +11,32 @@ public class AppDbContext : DbContext
     }
 
 
-    public DbSet<Egitim> Egitimler { get; set; } = null!;
-    public DbSet<Video> Videolar { get; set; } = null!;
-    public DbSet<VideoTranscript> VideoTranscripts { get; set; } = null!;
-    public DbSet<VideoSummary> VideoSummaries { get; set; } = null!;
-    public DbSet<ContentRequest> ContentRequests { get; set; } = null!;
-    public DbSet<GeneratedContent> GeneratedContents { get; set; } = null!;
-    public DbSet<QcResult> QcResults { get; set; } = null!;
-    public DbSet<PipelineLog> PipelineLogs { get; set; } = null!;
-    public DbSet<VideoChunkDocument> VideoChunkDocuments { get; set; } = null!;
+    public virtual DbSet<Egitim> Egitimler { get; set; } = null!;
+    public virtual DbSet<Video> Videolar { get; set; } = null!;
+    public virtual DbSet<VideoTranscript> VideoTranscripts { get; set; } = null!;
+    public virtual DbSet<VideoSummary> VideoSummaries { get; set; } = null!;
+    public virtual DbSet<ContentRequest> ContentRequests { get; set; } = null!;
+    public virtual DbSet<GeneratedContent> GeneratedContents { get; set; } = null!;
+    public virtual DbSet<QcResult> QcResults { get; set; } = null!;
+    public virtual DbSet<PipelineLog> PipelineLogs { get; set; } = null!;
+    public virtual DbSet<VideoChunkDocument> VideoChunkDocuments { get; set; } = null!;
     
-    public DbSet<EndpointLog> EndpointLogs { get; set; } = null!;
-    public DbSet<FunctionLog> FunctionLogs { get; set; } = null!;
+    public virtual DbSet<EndpointLog> EndpointLogs { get; set; } = null!;
+    public virtual DbSet<FunctionLog> FunctionLogs { get; set; } = null!;
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
         
-        // Add pgvector extension
-        modelBuilder.HasPostgresExtension("vector");
+        // Add pgvector extension if using PostgreSQL
+        if (Database.ProviderName == "Npgsql.EntityFrameworkCore.PostgreSQL")
+        {
+            modelBuilder.HasPostgresExtension("vector");
+        }
+        else if (Database.ProviderName == "Microsoft.EntityFrameworkCore.InMemory")
+        {
+            modelBuilder.Ignore<VideoChunkDocument>();
+        }
 
         // Apply all configurations in this assembly
         modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
